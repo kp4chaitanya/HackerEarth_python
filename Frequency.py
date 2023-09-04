b64288af3ac4aa006a423b1951c9e27e107ce2db
Chay <chay@Chays-MacBook-Pro.local>
2023-09-04 21:04:25 +0530

    Frequency Program

---
+++ Frequency.py
@@ -0,0 +1,27 @@
+def freq_string(Str1):
+    """ 
+    This Function calculates the frequency of the words
+    Input: String of words
+    Output: Dictionary with frequency of words
+
+    """
+
+    print("Start executing")
+    
+    #String to list conversion
+    l1=Str1.split()
+    #List elements
+    print(l1)
+    # Using dictionary
+    count = {}
+    for i in l1:
+        if i not in count:
+            count[i] = 0
+        count[i] += 1
+
+    return count   
+
+#Input string
+s1='Chaitanya is the king, disha is the queen, Data scientist is his role'
+#Function call
+print(freq_string(s1))
